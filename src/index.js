5143af84b8517a8c1a99619f2f8a9209f9207e3c
Oleksandr Chukhlib <Chukhlib.aleksandr@gmail.com>
2021-11-16 15:58:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import './sass/main.scss';
 
-// // Smooth animation script for page navigation
-// const smoothLinks = document.querySelectorAll('a[href^="#"]');
-//     for (let smoothLink of smoothLinks) {
-//       smoothLink.addEventListener('click', function (e) {
-//         e.preventDefault();
-//         const id = smoothLink.getAttribute('href');
+// Smooth animation script for page navigation
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+    for (let smoothLink of smoothLinks) {
+      smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
 
-//         document.querySelector(id).scrollIntoView({
-//           behavior: 'smooth',
-//           block: 'start'
-//         });
-//       });
-//     };
+        document.querySelector(id).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      });
+    };
